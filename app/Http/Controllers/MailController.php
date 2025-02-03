42e7ff01a03e6903b256c1bb7833c88954f8db7d
freitas <maxhenrique308@gmail.com>
2025-02-03 13:40:26 -0300

    reativação do servidor de email para montagem da defesa

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -11,27 +11,16 @@ class MailController extends Controller
     public static function send($para,$assunto,$view,$contentView){
         //CONFIGURAÇÕES DE SERVIDOR
         $mail = new PHPMailer(true);
-        $mail->SMTPOptions = array(
-            'ssl' => array(
-                'verify_peer' => false,
-                'verify_peer_name' => false,
-                'allow_self_signed' => true
-             )
-         );
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        //dd($contentView);
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true; //Enable SMTP authentication
-        $mail->CharSet = 'UTF-8'; //charset
-                                   
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'maxhenrique308@gmail.com';                     //SMTP username
-        $mail->SMTPKeepAlive = true; // Mantém a conexão SMTP viva para envios subsequentes
         $mail->Password   = 'xqkdmpvgokcqahrr';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465; 
-        $mail->Timeout = 120; // 120 segundos
         //DESTINATÁRIO
-        $mail->setFrom('maxhenrique308@gmail.com', 'FREventos'); //Rementente
+        $mail->setFrom('maxhenrique308@gmail.com', 'FR Tecnologia'); //Rementente
         $mail->addAddress($para, 'Inscrito');     //Destinatário
         //Corpo
         $mail->isHTML(true);  // Seta o formato do e-mail para aceitar conteúdo HTML
